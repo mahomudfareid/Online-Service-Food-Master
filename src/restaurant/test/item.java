170efd220360a23dc071f02f2d2df45c5164e701
mahomudfareid <102368407+mahomudfareid@users.noreply.github.com>
2022-05-18 04:12:21 +0200

    Update item.java

--- src/restaurant/test/item.java
+++ src/restaurant/test/item.java
@@ -1,37 +1,47 @@
-public class Item {
-    private int id;
-    private String name;
-    private String price;
+package restaurant.test;
 
-    public Item(){};
+public class item {
+    private int item_id;
+    private String item_name;
+    private String item_price;
 
-    public Item(int id, String name, String price){
-        this.id = id;
-        this.name = name;
-        this.price = price;
+    public item(){};
+
+    public item(int item_id, String item_name, String item_price){
+        this.item_id = item_id;
+        this.item_name = item_name;
+        this.item_price = item_price;
     }
 
+    item(String string, String string0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    //item(String string, String string0) {
+      //  throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  //  }
+
     public int getId() {
-        return id;
+        return item_id;
     }
 
     public String getName() {
-        return name;
+        return item_name;
     }
 
     public String getPrice() {
-        return price;
+        return item_price;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId(int item_id) {
+        this.item_id = item_id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String item_name) {
+        this.item_name = item_name;
     }
 
-    public void setPrice(String price) {
-        this.price = price;
+    public void setPrice(String item_price) {
+        this.item_price = item_price;
     }
 }
